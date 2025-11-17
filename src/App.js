d1e71157a56daa0bc734eda6ca12739c5de4d6a1
Asad <asad@Asads-Mac-mini.local>
2025-11-17 13:05:29 +0600

    Inital React App fir Jenkins pipeline

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px", fontSize: "24px" }}>
+      <h1>Welcome to My Simple React App!</h1>
+      <p>Jenkins Pipeline Build Successful 🚀</p>
+    </div>
+  );
+}
+
+export default App;
